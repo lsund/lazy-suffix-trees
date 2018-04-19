c178fb1d30968d884b4d9f64c66a9e2df8c3f236
Ludvig Sundström <lud.sund@gmail.com>
2018-04-19 21:18:51 +0200

    Add a test for correct count of patterns

--- include/occurs.h
+++ include/occurs.h
@@ -14,55 +14,12 @@ typedef struct result {
     bool val;
 } Result;
 
+
 typedef struct pattern {
     wchar_t head;
     wchar_t *probe;
     wchar_t *end;
 } Pattern;
-
-
-///////////////////////////////////////////////////////////////////////////////
-// Macros
-
-
-// Checks if there is an a-edge out from the root
-#define CHECK_A_EDGE\
-    {\
-        if(IS_LEAF(&rootchild)) {\
-            text_probe = wtext + (rootchild & ~LEAFBIT);\
-            Uint prefix = lcp(patt_probe + 1, patt_end, text_probe + 1, sentinel - 1);\
-            if ((Uint) (patt_end - patt_probe) == prefix)\
-            {\
-              return true;\
-            }\
-            return false;\
-        }\
-        vertex = stree + rootchild;\
-     }
-
-
-// Tries to match the remainder of the pattern with the current leaf edge
-#define MATCH_LEAF_EDGE\
-        if(text_probe == sentinel)\
-        {\
-          return false;\
-        }\
-        edgechar = *text_probe;\
-        if(edgechar == patt_head)\
-        {\
-          if((Uint) (patt_end - patt_probe) ==\
-                     lcp(patt_probe+1,patt_end,text_probe+1,sentinel-1))\
-          {\
-            return true;\
-          }\
-          return false;\
-        }
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////
-// Functions
 
 
 bool search(wchar_t *left, wchar_t *patt_end);
--- test/test_search.c
+++ test/test_search.c
@@ -8,8 +8,7 @@ static Uint min(const Uint a, const Uint b)
     return a < b ? a : b;
 }
 
-
-char *utest_search_for(char *patternfile, char *textfile)
+char *test_count(char *patternfile, char *textfile, Uint count)
 {
     Uint patternslen;
     setlocale(LC_ALL, "en_US.utf8");
@@ -28,8 +27,50 @@ char *utest_search_for(char *patternfile, char *textfile)
     int npatterns  = file_to_strings(patternfile, &patternslen, MAX_PATTERNS, &patterns);
     inittree();
 
+    Uint exists_n = 0;
+    for (Uint j = 0; j < (Uint) npatterns; j++) {
+
+        wchar_t *current_pattern = patterns[j];
+        Uint patternlen = strlenw(current_pattern);
+
+        bool exists = try_search(current_pattern, patternlen);
+
+        exists ? exists_n++ : (void) 0;
+    }
+    for (int i = npatterns - 1; i >= 0; i--) {
+        free(patterns[i]);
+    }
+
+    free(patterns);
+
+    mu_assert("Count should be correct", exists_n == count);
+
+    return NULL;
+}
+
+char *compare_vs_naive(char *patternfile, char *textfile)
+{
+    int maxpatterns = 50;
+
+    Uint patternslen;
+    setlocale(LC_ALL, "en_US.utf8");
+    FILE *in = fopen(textfile, "r");
+    wtext = malloc(sizeof(wchar_t) * MAXTEXTLEN);
+    wint_t c;
+    textlen = 0;
+    while ((c = fgetwc(in)) != WEOF) {
+        wtext[textlen] = c;
+        textlen++;
+    }
+    wtext[textlen + 1] = '\0';
+    max_codepoint = get_max(wtext, textlen);
+    fclose(in);
+    wchar_t **patterns = (wchar_t **) malloc(sizeof(wchar_t *) * MAX_PATTERNS);
+    int npatterns  = file_to_strings(patternfile, &patternslen, MAX_PATTERNS, &patterns);
+    inittree();
+
     int exists_n = 0, rexists_n = 0;
-    for (Uint j = 0; j < min(npatterns, 20); j++) {
+    for (Uint j = 0; j < min(npatterns, maxpatterns); j++) {
 
         wchar_t *current_pattern = patterns[j];
         Uint patternlen = strlenw(current_pattern);
@@ -37,30 +78,23 @@ char *utest_search_for(char *patternfile, char *textfile)
         wchar_t *end = current_pattern + patternlen;
 
         bool exists = try_search(current_pattern, patternlen);
-        bool really_exists = naive_search(current_pattern, end);
+        bool rexists = naive_search(current_pattern, end);
 
-        printf("%d %d\n", really_exists, exists);
-        if (exists) {
-            exists_n++;
-        }
-        if (really_exists) {
-            rexists_n++;
-        }
-        if (really_exists) {
-        }
-        if (really_exists != exists) {
-            printf("%d %d\n", really_exists, exists);
+        exists ? exists_n++ : (void) 0;
+        rexists ? rexists_n++ : (void) 0;
+
+        if (rexists != exists) {
+            printf("%d %d\n", rexists, exists);
             printf("Fail on: %ls\n", patterns[j]);
         }
         mu_assert(
             "Naive and suffix tree sourch should be the same.",
-            really_exists == exists
+            rexists == exists
         );
     }
     for (int i = npatterns - 1; i >= 0; i--) {
         free(patterns[i]);
     }
-    printf("count: %d %d\n", exists_n, rexists_n);
     free(patterns);
     return NULL;
 }
@@ -70,31 +104,23 @@ char *utest_search()
 
     char *error;
 
-    /* mu_message(DATA, "Trivial\n"); */
-    /* error = utest_search_for("data/small-patt.txt", "data/small.txt"); */
-    /* if (error) return error; */
+    mu_message(DATA, "Random existing patterns\n");
+    error = compare_vs_naive("data/random-patterns.txt", "data/dataset/005.txt");
+    if (error) return error;
 
-    /* mu_message(DATA, "Easy test patterns\n"); */
-    /* error = utest_search_for("data/test-patterns.txt", "data/dataset/005.txt"); */
-    /* if (error) return error; */
-
-    /* mu_message(DATA, "Random existing patterns\n"); */
-    /* error = utest_search_for("data/random-patterns.txt", "data/dataset/005.txt"); */
-    /* if (error) return error; */
-
-    /* mu_message(DATA, "Random non-existing patterns\n"); */
-    /* error = utest_search_for( */
-    /*             "data/random-patterns-non-existing.txt", */
-    /*             "data/dataset/005.txt" */
-    /*         ); */
-    /* if (error) return error; */
-
-    /* mu_message(DATA, "Trivial AKZ patterns\n"); */
-    /* error = utest_search_for("data/akz/trivial.txt", "data/data.xml"); */
-    /* if (error) return error; */
+    mu_message(DATA, "Random non-existing patterns\n");
+    error = compare_vs_naive(
+                "data/random-patterns-non-existing.txt",
+                "data/dataset/005.txt"
+            );
+    if (error) return error;
 
     mu_message(DATA, "Akz patterns\n");
-    error = utest_search_for("data/10000.txt", "data/data.xml");
+    error = compare_vs_naive("data/10000.txt", "data/data.xml");
+    if (error) return error;
+
+    mu_message(DATA, "Count: Akz patterns\n");
+    error = test_count("data/10000.txt", "data/data.xml", 174);
     if (error) return error;
 
     return NULL;
